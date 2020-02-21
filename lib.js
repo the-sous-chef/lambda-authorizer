f394ccd171f11d3965aaf973af4210de4bd8b626
Ivan Santos <ivan.santos@vistaprint.com>
2020-02-21 11:42:16 +0100

    Add 1 day cache to signing keys

--- lib.js
+++ lib.js
@@ -1,93 +1,99 @@
-'use strict';
+"use strict";
 
-const jwksClient = require('jwks-rsa');
-const jwt = require('jsonwebtoken');
+const jwksClient = require("jwks-rsa");
+const jwt = require("jsonwebtoken");
 
-const getPolicyDocument = function (effect, resource) {
-    const policyDocument = {};
-    policyDocument.Version = '2012-10-17'; // default version
-    policyDocument.Statement = [];
-    const statementOne = {};
-    statementOne.Action = 'execute-api:Invoke'; // default action
-    statementOne.Effect = effect;
-    statementOne.Resource = resource;
-    policyDocument.Statement[0] = statementOne;
-    return policyDocument;
+const client = jwksClient({
+  cache: true,
+  cacheMaxAge: 86400000, //value in ms
+  rateLimit: true,
+  jwksRequestsPerMinute: 10,
+  strictSsl: true,
+  jwksUri: process.env.JWKS_URI
+});
+
+const getPolicyDocument = function(effect, resource) {
+  const policyDocument = {};
+  policyDocument.Version = "2012-10-17"; // default version
+  policyDocument.Statement = [];
+  const statementOne = {};
+  statementOne.Action = "execute-api:Invoke"; // default action
+  statementOne.Effect = effect;
+  statementOne.Resource = resource;
+  policyDocument.Statement[0] = statementOne;
+  return policyDocument;
 };
-
 
 // extract and return the Bearer Token from the Lambda event parameters
-const getToken = function (params) {
-    if (!params.type || params.type !== 'TOKEN') {
-        throw new Error("Expected 'event.type' parameter to have value TOKEN");
-    }
+const getToken = function(params) {
+  if (!params.type || params.type !== "TOKEN") {
+    throw new Error("Expected 'event.type' parameter to have value TOKEN");
+  }
 
-    const tokenString = params.authorizationToken;
-    if (!tokenString) {
-        throw new Error("Expected 'event.authorizationToken' parameter to be set");
-    }
+  const tokenString = params.authorizationToken;
+  if (!tokenString) {
+    throw new Error("Expected 'event.authorizationToken' parameter to be set");
+  }
 
-    const match = tokenString.match(/^Bearer (.*)$/i);
-    if (!match || match.length < 2) {
-        throw new Error("Invalid Authorization token - '" + tokenString + "' does not match 'Bearer .*' or 'bearer .*");
-    }
-    return match[1];
+  const match = tokenString.match(/^Bearer (.*)$/i);
+  if (!match || match.length < 2) {
+    throw new Error(
+      "Invalid Authorization token - '" +
+        tokenString +
+        "' does not match 'Bearer .*' or 'bearer .*"
+    );
+  }
+  return match[1];
 };
 
-module.exports.authenticate = function (params, cb) {
-    const token = getToken(params);
+module.exports.authenticate = function(params, cb) {
+  const token = getToken(params);
 
-    const client = jwksClient({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 10, // Default value
-        jwksUri: process.env.JWKS_URI
-    });
-
-    const decoded = jwt.decode(token, {complete: true});
-    if (decoded === null)
-    {
-        console.error("Could not decode JWT.");
-        cb("Could not decode JWT.");
-    }
-    const kid = decoded.header.kid;
-    client.getSigningKey(kid, function (err, key) {
-        if(err)
-        {
+  const decoded = jwt.decode(token, { complete: true });
+  if (decoded === null) {
+    console.error("Could not decode JWT.");
+    cb("Could not decode JWT.");
+  }
+  const kid = decoded.header.kid;
+  client.getSigningKey(kid, function(err, key) {
+    if (err) {
+      console.error(err.message);
+      cb(err);
+    } else {
+      const signingKey = key.publicKey || key.rsaPublicKey;
+      jwt.verify(
+        token,
+        signingKey,
+        { audience: process.env.AUDIENCE, issuer: process.env.TOKEN_ISSUER },
+        function(err, decoded) {
+          if (err) {
             console.error(err.message);
             cb(err);
-        }
-        else
-        {
-            const signingKey = key.publicKey || key.rsaPublicKey;
-            jwt.verify(token, signingKey, { audience: process.env.AUDIENCE, issuer: process.env.TOKEN_ISSUER },
-            function (err, decoded) {
-                if (err) {
-                    console.error(err.message);
-                    cb(err);
+          } else {
+            // Make sure that the user is a cimpress employee
+            const accountClaim = decoded["https://claims.cimpress.io/account"];
+            const validAccounts = [
+              "g2Ez5VaoZWoqU22XqPjTLU", // Cimpress Technology
+              "ozoDdrmewShEcbUDWX8J3V" // Vistaprint
+            ];
+            if (validAccounts.includes(accountClaim)) {
+              cb(null, {
+                principalId: decoded.sub,
+                policyDocument: getPolicyDocument("Allow", params.methodArn),
+                context: {
+                  scope: decoded.scope
                 }
-                else {
-                    // Make sure that the user is a cimpress employee
-                    const accountClaim = decoded['https://claims.cimpress.io/account'];
-                    const validAccounts = [
-                        'g2Ez5VaoZWoqU22XqPjTLU', // Cimpress Technology
-                        'ozoDdrmewShEcbUDWX8J3V', // Vistaprint
-                    ];
-                    if(validAccounts.includes(accountClaim)) {
-                        cb(null, {
-                            principalId: decoded.sub,
-                            policyDocument: getPolicyDocument('Allow', params.methodArn),
-                            context: {
-                                scope: decoded.scope
-                            }
-                        });
-                    }
-                    else {
-                        console.error(decoded.sub + " is not authorized to access this endpoint as they are not a Cimpress User.");
-                        cb("The user is not authorized to access this endpoint.");
-                    }
-                }
-            });
+              });
+            } else {
+              console.error(
+                decoded.sub +
+                  " is not authorized to access this endpoint as they are not a Cimpress User."
+              );
+              cb("The user is not authorized to access this endpoint.");
+            }
+          }
         }
-    });
+      );
+    }
+  });
 };
